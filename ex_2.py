bd43514df343ea70300611c937338b30d774ed2d
Toxa <asemenov42@yandex.ru>
2016-10-26 19:18:15 +0300

    Otchet

--- ex_2.py
+++ ex_2.py
@@ -5,6 +5,6 @@ from librip.iterators import Unique
 data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
 data2 = gen_random(1, 3, 10)
 un=Unique(data2)
-print(list(un))
+print(list(data1))
 
 # Реализация задания 2
